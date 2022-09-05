fd75dac49eda6fc8483bb87c5808460acfc95687
loic <loic.martel@outlook.com>
2022-09-05 13:41:35 +0000

    Decode Required and optional fields

--- beggining-elm/src/PlaygroundApp.elm
+++ beggining-elm/src/PlaygroundApp.elm
@@ -5,7 +5,6 @@ import Html exposing (Html, button, div, h3, li, text, ul)
 import Html.Attributes exposing (type_)
 import Html.Events exposing (onClick)
 import Http
-
 import Json.Decode exposing (Decoder, Error(..), decodeString, list, string)
 
 
@@ -17,38 +16,46 @@ type alias ShoppingList =
 
 type Msg
     = SendHttpRequest
-    | DataReceived (Result Http.Error ( List String) )
+    | DataReceived (Result Http.Error (List String))
+
 
 view : ShoppingList -> Html Msg
 view shopList =
     div []
         [ button [ onClick SendHttpRequest, type_ "button" ]
             [ text "Obtenir la liste des courses." ]
-        , viewArticlesOrError shopList        ]
+        , viewArticlesOrError shopList
+        ]
+
 
 viewArticlesOrError : ShoppingList -> Html Msg
-viewArticlesOrError model = 
-  case model.errorMessages of 
-    Just message ->
-      viewError message 
-    Nothing ->
-      viewArticles model.articles
+viewArticlesOrError model =
+    case model.errorMessages of
+        Just message ->
+            viewError message
+
+        Nothing ->
+            viewArticles model.articles
+
 
 viewError : String -> Html Msg
 viewError errorMessage =
-  let errorHeading = "Could not fetch articles"
-  in div [] 
-    [ h3 [] [text errorHeading]
-    , text ( "Error:" ++ errorMessage)]
+    let
+        errorHeading =
+            "Could not fetch articles"
+    in
+    div []
+        [ h3 [] [ text errorHeading ]
+        , text ("Error:" ++ errorMessage)
+        ]
+
 
 viewArticles : List String -> Html Msg
-viewArticles shopList = 
-  div [] [
-    h3 [] [ text "List of articles"]
-    , ul [] ( List.map ( \y -> li [] [ text y] ) shopList)
-  ]
-
-
+viewArticles shopList =
+    div []
+        [ h3 [] [ text "List of articles" ]
+        , ul [] (List.map (\y -> li [] [ text y ]) shopList)
+        ]
 
 
 url : String
@@ -58,17 +65,22 @@ url =
 
 buildErrorMessages : Http.Error -> String
 buildErrorMessages msg =
-  case msg of
-    Http.BadUrl message ->
-      message
-    Http.Timeout ->
-      "Request timed Out"
-    Http.BadBody message ->
-      "Error on the Body" ++ "\n" ++ message
-    Http.NetworkError ->
-      "Network not available."
-    Http.BadStatus message ->
-      "Wrong status" ++ ( String.fromInt message ) ++ " Not expected"
+    case msg of
+        Http.BadUrl message ->
+            message
+
+        Http.Timeout ->
+            "Request timed Out"
+
+        Http.BadBody message ->
+            "Error on the Body" ++ "\n" ++ message
+
+        Http.NetworkError ->
+            "Network not available."
+
+        Http.BadStatus message ->
+            "Wrong status" ++ String.fromInt message ++ " Not expected"
+
 
 getArticles : Cmd Msg
 getArticles =
@@ -77,18 +89,20 @@ getArticles =
         , expect = Http.expectJson DataReceived articlesDecoder
         }
 
+
 articlesDecoder : Decoder (List String)
 articlesDecoder =
-   list string
+    list string
+
 
 handleJsonError : Json.Decode.Error -> Maybe String
 handleJsonError err =
-  case err of 
-    Failure errMessage _ ->
-      Just errMessage
-    _ ->
-      Just "Generic Json error. For more detail, please implement it"
-      
+    case err of
+        Failure errMessage _ ->
+            Just errMessage
+
+        _ ->
+            Just "Generic Json error. For more detail, please implement it"
 
 
 update : Msg -> ShoppingList -> ( ShoppingList, Cmd Msg )
@@ -101,7 +115,7 @@ update msg model =
             ( { model | articles = articles }, Cmd.none )
 
         DataReceived (Err httpError) ->
-            ( { model | errorMessages = Just (buildErrorMessages httpError) } , Cmd.none )
+            ( { model | errorMessages = Just (buildErrorMessages httpError) }, Cmd.none )
 
 
 main : Program () ShoppingList Msg
